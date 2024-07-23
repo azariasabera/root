2cd56483e7dca02b38e70e16ac5a26b263886216
Azarias Galama <129897944+azariasabera@users.noreply.github.com>
2024-07-23 18:04:08 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use("/api", router);
+app.use("/api", indexRouter);
 
 
 module.exports = app;
